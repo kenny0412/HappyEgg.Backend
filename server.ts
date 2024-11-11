cdf73a02880df9c736efd41bb52fe00c1bf66115
Kenny Cardenas <kennyc680@gmail.com>
2024-11-11 12:36:07 -0600

    fix-happy-01: Fix server issues when running

--- server.ts
+++ server.ts
@@ -1,4 +1,4 @@
-import app from './src/app'
+import app from './src/app.js'
 
 app.use((_, res, next) => {
   // Access domain allowed
@@ -13,9 +13,9 @@ app.use((_, res, next) => {
   next()
 })
 /**
- ** Server Activation
- */
-const PORT = process.env.PORT ?? 0
+     ** Server Activation
+     */
+const PORT = process.env.PORT ?? 3000
 app.listen(PORT, () => {
   console.log(`Server listening on http://localhost:${PORT}`)
 })
